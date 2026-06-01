614c270dd4f7fbd3bec06d939ed7a486e8b72184
Pablo López <lopez.pablo2305@gmail.com>
2026-06-01 02:17:18 -0400

    fix: cors

--- core/settings.py
+++ core/settings.py
@@ -58,6 +58,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://react-tercera.vercel.app",
     "https://stage.terceraquillota.cl",
+    "http://stage.terceraquillota.cl",
 ]
 
 REST_FRAMEWORK = {
